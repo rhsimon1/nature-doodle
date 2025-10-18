dc6ae35f67617456d21bddf631bb070ea312d400
rhsimon1 <simonly1996@hotmail.com>
2025-10-18 16:13:03 -0500

    Created Sign Sign out

--- src/app/api/file/route.js
+++ src/app/api/file/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { pinata } from '../../../../utils/config';
-import { TrackOpTypes } from 'vue';
+ 
 
 export async function POST(request) {
   try {
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,14 @@
 import './globals.css';
 import Navbar from './components/NavBar';
 import { Comfortaa } from 'next/font/google';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
 const comfortaa = Comfortaa({
   subsets: ['latin'],
@@ -16,11 +24,27 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`light ${comfortaa.variable}`}>
-      <body>
-        <Navbar></Navbar>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`light ${comfortaa.variable}`}>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </header>
+
+        <body>
+          <Navbar></Navbar>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }

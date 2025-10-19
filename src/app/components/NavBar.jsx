f4aaa527d099a2264e2b31c299c8c4bd41a9edf8
rhsimon1 <simonly1996@hotmail.com>
2025-10-18 19:57:32 -0500

    Header and Navbar color change

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
     <div className="fixed top-0 left-0 h-screen">
-      <nav className="flex flex-col gap-5 p-10 bg-yellow-200 h-full w-40">
+      <nav className="flex flex-col gap-5 p-10 bg-tertiary-container h-full w-40">
         <Link
           href="/"
           className="w-full px-4 py-2 rounded-lg hover:bg-yellow-300 transition-colors"
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,20 +26,6 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en" className={`light ${comfortaa.variable}`}>
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <SignedOut>
-            <SignInButton />
-            <SignUpButton>
-              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </header>
-
         <body>
           <Header></Header>
           <Navbar></Navbar>

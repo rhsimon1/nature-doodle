167cf8e7e35553d2225b1ec064a221547291e78d
Dannie6 <danielperlera1@gmail.com>
2025-10-18 22:00:36 -0500

    Sign In/Out Buttons
    Fixed into row and coloring

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -10,8 +10,8 @@ import {
 } from '@clerk/nextjs';
 export default function Header() {
   return (
-    <header className="flex items-center justify-center px-6 py-6 bg-primary ">
-      <Link href="/" className="flex justify-start w-full">
+    <header className="flex items-center justify-between px-6 py-6 bg-primary w-full">
+      <Link href="/" className="flex-shrink-0">
         <Image
           src="/logo.svg"
           alt="Nature Doodle Logo"
@@ -20,19 +20,25 @@ export default function Header() {
           priority
         />
       </Link>
-      <header className="flex justify-end items-center p-4 gap-4 h-16">
+      <div className="flex items-center gap-4">
         <SignedOut>
-          <SignInButton />
-          <SignUpButton>
-            <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-              Sign Up
-            </button>
-          </SignUpButton>
+          <div className="flex gap-4 items-center">
+            <SignInButton> 
+              <button className="hover:bg-outline transition-colors text-ceramic-white rounded-md font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-8 cursor-pointer">
+                Sign In
+              </button>
+            </SignInButton>
+            <SignUpButton>
+              <button className="hover:bg-outline transition-colors text-ceramic-white rounded-md font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-8 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </div>
         </SignedOut>
         <SignedIn>
           <UserButton />
         </SignedIn>
-      </header>
+      </div>
     </header>
   );
 }
